4ef1d79ba508b1fa958247ee600eef2ad8a21ab4
Christoph Braun <christoph.braun@protonmail.com>
2023-12-13 15:28:11 +0100

    fix cors issue, and rename header

--- src/main/java/edu/kit/aifb/websci/MyExampleServlet.java
+++ src/main/java/edu/kit/aifb/websci/MyExampleServlet.java
@@ -1,11 +1,5 @@
 package edu.kit.aifb.websci;
 
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.HeaderParam;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.MediaType;
-import jakarta.ws.rs.core.Response;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
@@ -21,39 +15,39 @@ import org.apache.jena.shacl.Shapes;
 import org.apache.jena.shacl.ValidationReport;
 import org.apache.jena.shacl.lib.ShLib;
 
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.HeaderParam;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 @Path("/resource") // Specify the URL pattern for the servlet. Remember that this is relative to
-                   // the application path defined in `pom.xml`
+// the application path defined in `pom.xml`
 public class MyExampleServlet {
 
-    String pathPolicyR_offer = "/Users/vbetz/Code/dua_server-2/src/data/policies/PolicyR_offer.trig";
-    String pathSHACL_vp = "/Users/vbetz/Code/dua_server-2/src/data/shapes/SHACL_vp.ttl";
-    String pathVP = "/Users/vbetz/Code/dua_server-2/src/data/credentials/VP.trig";
-    String pathGRAPH_test = "/Users/vbetz/Code/dua_server-2/src/data/credentials/GRAPH_test.ttl";
-    String pathSHACL_test = "/Users/vbetz/Code/dua_server-2/src/data/shapes/SHACL_test.ttl";
-    String pathSHACL_VP_TTL_Policies = "/Users/vbetz/Code/dua_server-2/src/data/shapes/SHACL_vp_TTL_policies.ttl";
-    String pathAgreements = "/Users/vbetz/Code/dua_server-2/src/data/policies/Policy_agreements.trig";
+    String pathPolicyR_offer = "./src/data/policies/PolicyR_offer.trig";
+    String pathSHACL_vp = "./src/data/shapes/SHACL_vp.ttl";
+    String pathVP = "./src/data/credentials/VP.trig";
+    String pathGRAPH_test = "./src/data/credentials/GRAPH_test.ttl";
+    String pathSHACL_test = "./src/data/shapes/SHACL_test.ttl";
+    String pathSHACL_VP_TTL_Policies = "./src/data/shapes/SHACL_vp_TTL_policies.ttl";
+    String pathAgreements = "./src/data/policies/Policy_agreements.trig";
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public Response getResource(@HeaderParam("RDF-Data") String authString) {
+    public Response getResource(@HeaderParam("authn-data") String authString) {
 
         System.out.println(authString);
 
         String pathSHACL = pathSHACL_test;
 
-        // System.out.println(authString);
         if (authString == null) {
             return Response.status(Response.Status.UNAUTHORIZED)
                     .entity("Provide matching Verifiable Presentation.")
                     .header("WWW-Authenticate",
                             readFile(pathSHACL_VP_TTL_Policies))
                     .header("Content-Type", "text/html")
-                    .header("Access-Control-Allow-Origin", "*")
-                    .header("Access-Control-Allow-Methods", "GET, POST, HEAD, PUT, OPTIONS")
-                    .header("Access-Control-Allow-Headers", "RDF-Data, origin, content-type, accept, authorization")
-                    .header("Access-Control-Allow-Credentials", "true")
-                    .header("Access-Control-Max-Age", "1209600")
                     .build();
         }
 
@@ -64,11 +58,6 @@ public class MyExampleServlet {
                                 readFile(pathAgreements))
                         // .header("Content-Type", "application/trig")
                         .header("Content-Type", "text/html")
-                        .header("Access-Control-Allow-Origin", "*")
-                        .header("Access-Control-Allow-Methods", "GET, POST, HEAD, PUT, OPTIONS")
-                        .header("Access-Control-Allow-Headers", "RDF-Data, origin, content-type, accept, authorization")
-                        .header("Access-Control-Allow-Credentials", "true")
-                        .header("Access-Control-Max-Age", "1209600")
                         .build();
             }
         }
@@ -78,11 +67,6 @@ public class MyExampleServlet {
                 .header("WWW-Authenticate",
                         readFile(pathSHACL_VP_TTL_Policies))
                 .header("Content-Type", "text/html")
-                .header("Access-Control-Allow-Origin", "*")
-                .header("Access-Control-Allow-Methods", "GET, POST, HEAD, PUT, OPTIONS")
-                .header("Access-Control-Allow-Headers", "RDF-Data, origin, content-type, accept, authorization")
-                .header("Access-Control-Allow-Credentials", "true")
-                .header("Access-Control-Max-Age", "1209600")
                 .build();
 
     }
@@ -136,6 +120,3 @@ public class MyExampleServlet {
     }
 
 }
-
-
-
--- src/main/java/edu/kit/aifb/websci/MyServletContextListener.java
+++ src/main/java/edu/kit/aifb/websci/MyServletContextListener.java
@@ -2,7 +2,6 @@ package edu.kit.aifb.websci;
 
 import org.glassfish.jersey.server.ServerProperties;
 import org.glassfish.jersey.servlet.ServletContainer;
-// import org.glassfish.jersey.server.ResourceConfig;
 
 import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletContextEvent;
@@ -10,11 +9,13 @@ import jakarta.servlet.ServletContextListener;
 import jakarta.servlet.ServletRegistration;
 import jakarta.servlet.annotation.WebListener;
 
+import jakarta.servlet.FilterRegistration;
+import jakarta.ws.rs.HttpMethod;
+
 @WebListener
 public class MyServletContextListener implements ServletContextListener {
 
     ServletContext ctx;
-    
 
     /**
      * On startup of context (on server up), register your servlets.
@@ -25,6 +26,12 @@ public class MyServletContextListener implements ServletContextListener {
         ServletRegistration sr = ctx.addServlet("MyExampleServlet", ServletContainer.class);
         sr.addMapping("/*"); // no idea why wildcard is needed, but it is what it is.
         sr.setInitParameter(ServerProperties.PROVIDER_PACKAGES, this.getClass().getPackage().toString());
+        FilterRegistration fr;
+        // Register and configure filter to handle CORS requests
+        fr = ctx.addFilter("cross-origin", org.eclipse.jetty.servlets.CrossOriginFilter.class.getName());
+        fr.setInitParameter(org.eclipse.jetty.servlets.CrossOriginFilter.ALLOWED_METHODS_PARAM,
+                HttpMethod.GET + "," + HttpMethod.PUT + "," + HttpMethod.POST + "," + HttpMethod.DELETE + "," + HttpMethod.OPTIONS + "," + HttpMethod.HEAD);
+        fr.setInitParameter(org.eclipse.jetty.servlets.CrossOriginFilter.ALLOWED_HEADERS_PARAM , "authn-data");
+        fr.addMappingForUrlPatterns(null, true, "/*");
     }
 }
-
