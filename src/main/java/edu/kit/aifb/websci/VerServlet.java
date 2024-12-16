8669f155aee11621ee8c08bdf58e7fc486131525
vallebz <80699091+vallebz@users.noreply.github.com>
2024-12-16 19:38:45 +0100

    added dynamic path; edited session handling; edited shape and vp

--- src/main/java/edu/kit/aifb/websci/VerServlet.java
+++ src/main/java/edu/kit/aifb/websci/VerServlet.java
@@ -4,8 +4,11 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.StringReader;
 import java.io.StringWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -13,8 +16,10 @@ import java.util.Base64;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
@@ -30,7 +35,6 @@ import jakarta.ws.rs.core.UriInfo;
 import jakarta.ws.rs.core.NewCookie;
 import jakarta.ws.rs.core.NewCookie.SameSite;
 
-import org.apache.jena.base.Sys;
 import org.apache.jena.graph.Graph;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
@@ -52,83 +56,147 @@ public class VerServlet {
 
     static {
         try {
-            // Load SHACL shapes from a file and add them to the resource "resource".
+            // For DEMO purposes, we load a SHACL shape from a file and add it to the
+            // resources
+            // we have on the solid pod container
+            // "https://sme.solid.aifb.kit.edu/bookings/".
             String shapeContent = new String(Files.readAllBytes(Paths.get("./src/data/shapes/example_vp_shape.ttl")));
-            AuthService.createResource("resource");
-            AuthService.addShapeToResource("resource", parseRDF(shapeContent, Lang.TTL));
+            AuthService.createResource("0.ttl");
+            AuthService.createResource("readme.ttl");
+            AuthService.addShapeToResource("0.ttl", parseRDF(shapeContent, Lang.TTL));
+            AuthService.addShapeToResource("readme.ttl", parseRDF(shapeContent, Lang.TTL));
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    @GET
-    @Path("/resource")
-    @Produces(MediaType.TEXT_PLAIN)
-    public Response getResource(@Context UriInfo uriInfo) {
-        // Retrieve the nonce parameter from the request.
-        String nonce = uriInfo.getQueryParameters().getFirst("nonce");
-        String path = uriInfo.getPath(); // Gets the requested resource path.
+    // Checks if the nonce is present and not empty.
+    private boolean hasNonce(String nonce) {
+        return nonce != null && !nonce.isEmpty();
+    }
 
-        // If the nonce parameter is missing or invalid, return 400 Bad Request.
-        if (nonce == null || nonce.isEmpty()) {
+    // Checks if the session cookie is present.
+    private boolean hasSessionCookie(Map<String, Cookie> cookies) {
+        return !cookies.isEmpty() && cookies.containsKey("JSESSIONID");
+    }
+
+    // Validates the session based on equivalency of nonce and session cookie.
+    private boolean isValidSession(String nonce, Map<String, Cookie> cookies) {
+        if (hasNonce(nonce) && hasSessionCookie(cookies)) {
+            return nonce.equals(cookies.get("JSESSIONID").getValue());
+        }
+        return hasNonce(nonce) || hasSessionCookie(cookies);
+    }
+
+    // Retrieves the session ID from nonce or cookies.
+    private String getSessionID(String nonce, Map<String, Cookie> cookies) {
+        if (hasNonce(nonce)) {
+            return nonce;
+        }
+        if (hasSessionCookie(cookies)) {
+            return cookies.get("JSESSIONID").getValue();
+        }
+        return null;
+    }
+
+    @GET
+    @Path("/getResource/{subPath: [[a-zA-Z0-9_/.]+[a-zA-Z0-9_/.]*]*}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response getResource(@Context UriInfo uriInfo, @Context HttpHeaders headers,
+            @PathParam("subPath") String subPath) {
+        String nonce = uriInfo.getQueryParameters().getFirst("nonce"); // Retrieve the nonce parameter from the request.
+        Map<String, Cookie> cookies = headers.getCookies(); // Retrieves the cookies from the request.
+
+        // If the resource parameter is missing or invalid, return 400 Bad Request.
+        if (subPath == null || subPath.isEmpty()) {
+            return Response.status(Response.Status.BAD_REQUEST).entity("Error: No resource provided.")
+                    .build();
+        }
+        String file = "https://sme.solid.aifb.kit.edu/bookings/" + subPath;
+
+        // If the session is not valid, create a new session and return 401
+        // Unauthorized.
+        if (!isValidSession(nonce, cookies)) {
             String sessionID = SessionService.createSession(); // Create a new session.
+            NewCookie sessionCookie = new NewCookie.Builder("JSESSIONID")
+                    .path("/")
+                    .maxAge(3600)
+                    .value(sessionID)
+                    .secure(true)
+                    .httpOnly(true)
+                    .sameSite(SameSite.STRICT)
+                    .build();
+
             return Response.status(Response.Status.UNAUTHORIZED)
-                    .entity("Error: Nonce parameter is missing or invalid.")
+                    .entity("Error: Invalid or missing session ID.")
                     .link("http://localhost:8080/auth/request?res="
-                            + Base64.getEncoder().encodeToString(path.getBytes())
+                            + Base64.getEncoder().encodeToString(subPath.getBytes())
                             + "&nonce=" + sessionID, "OID4VC-access-request")
+                    .cookie(sessionCookie)
                     .build();
         }
 
+        String sessionID = getSessionID(nonce, cookies);
+
         // If the session is not authorized for the requested resource, return 401
         // Unauthorized.
-        if (!SessionService.isSessionAuthorizedForResource(nonce, path)) {
+        if (!SessionService.isSessionAuthorizedForResource(sessionID, subPath)) {
             return Response.status(Response.Status.UNAUTHORIZED)
+                    .entity("Error: Session ID is not authorized to access this resource. Please authenticate with your credentials.")
                     .link("http://localhost:8080/auth/request?res="
-                            + Base64.getEncoder().encodeToString(path.getBytes()), "OID4VC-access-request")
+                            + Base64.getEncoder().encodeToString(subPath.getBytes()), "OID4VC-access-request")
                     .header("Content-Type", "text/html")
                     .build();
         }
 
-        // If the session is authorized, return the requested resource.
-        return Response.ok()
-                .entity("Congratulations, here is your requested resource! Authorized session: "
-                        + nonce)
-                .header("Content-Type", "text/html")
-                .build();
+        try {
+            // Create a URL connection
+            URL url = new URL(file);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            // Read the response
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String content = in.lines().collect(Collectors.joining("\n"));
+
+            // Close the connections
+            in.close();
+            connection.disconnect();
+
+            // If the session is authorized, return the requested resource.
+            return Response.ok()
+                    .entity(content)
+                    .header("Content-Type", "text/html")
+                    .build();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                    .entity("Fehler beim Abrufen der Ressource: " + e.getMessage())
+                    .header("Content-Type", "text/html")
+                    .build();
+        }
     }
 
     @GET
-    @Path("/auth/request")
+    @Path("/auth/request/{subPath: [[a-zA-Z0-9_/.]+[a-zA-Z0-9_/.]*]*}")
     @Produces("text/turtle")
-    public Response authRequest(@Context UriInfo uriInfo, @QueryParam("res") String base64EncodedResource) {
+    public Response authRequest(@Context UriInfo uriInfo, @PathParam("subPath") String subPath) {
 
-        String nonce = uriInfo.getQueryParameters().getFirst("nonce");
-        // If no or invalid sessionID is provided, return 400 Bad Request.
-        if (nonce == null || !session_presentations.containsKey(nonce)) {
-            return Response.status(Response.Status.BAD_REQUEST)
-                    .entity("Error: Invalid or no sessionID provided.")
-                    .build();
-        }
         // If the resource parameter is missing or invalid, return 400 Bad Request.
-        if (base64EncodedResource == null || base64EncodedResource.isEmpty()) {
+        if (subPath == null || subPath.isEmpty()) {
             return Response.status(Response.Status.BAD_REQUEST).entity("Error: No resource provided.")
                     .build();
         }
 
-        // Decode the base64-encoded resource path.
-        byte[] decodedBytes = Base64.getDecoder().decode(base64EncodedResource);
-        String decodedResource = new String(decodedBytes);
-
         // If the resource does not exist, return 404 Not Found.
-        if (AuthService.getShapesForResource(decodedResource) == null) {
+        if (AuthService.getShapesForResource(subPath) == null) {
             return Response.status(Response.Status.NOT_FOUND)
                     .build();
         }
 
         // Collect all SHACL shapes for the resource and return them.
         StringBuilder allShapes = new StringBuilder();
-        ArrayList<Model> tmp_shapes = AuthService.getShapesForResource(decodedResource);
+        ArrayList<Model> tmp_shapes = AuthService.getShapesForResource(subPath);
         for (Model shape : tmp_shapes) {
             StringWriter writer = new StringWriter();
             shape.write(writer, "TURTLE");
@@ -138,40 +206,41 @@ public class VerServlet {
 
         return Response.ok(allShapes.toString())
                 .header("Content-Type", "text/turtle")
-                .link("http://localhost:8080/auth/present?nonce=" + nonce, "OID4VC-access-presentation")
+                .link("http://localhost:8080/auth/present/" + subPath, "OID4VC-access-presentation")
                 .build();
     }
 
     @POST
     @Path("/auth/present")
     @Consumes("text/plain")
-    public Response authPresent(@Context UriInfo uriInfo, @Context HttpServletRequest request, String input) {
+    public Response authPresent(@Context UriInfo uriInfo, @Context HttpHeaders headers,
+            @Context HttpServletRequest request, String input) {
 
-        // Retrieve the nonce parameter from the request.
+        Map<String, Cookie> cookies = headers.getCookies(); // Retrieves the cookies from the request.
         String nonce = uriInfo.getQueryParameters().getFirst("nonce");
 
-        // If no or invalid sessionID is provided, return 400 Bad Request.
-        if (nonce == null || !session_presentations.containsKey(nonce)) {
+        // If the session is not valid, return 400 Bad Request.
+        if (!isValidSession(nonce, cookies)) {
             return Response.status(Response.Status.BAD_REQUEST)
                     .entity("Invalid or no sessionID provided")
                     .build();
         }
 
+        String sessionID = getSessionID(nonce, cookies);
+
         // Read the request body containing the Verifiable Presentation (VP).
-        String vpString;
-        vpString = input;
+        String vpString = input;
 
         // Parse the VP from the request body into a Jena Model.
         Model vpModel = parseRDF(vpString, Lang.TURTLE);
 
         // Add the VP to the session.
-        ArrayList<Model> tmp_presentations = SessionService.getPresentationsForSession(nonce);
+        ArrayList<Model> tmp_presentations = SessionService.getPresentationsForSession(sessionID);
         tmp_presentations.add(vpModel);
-        SessionService.addToSession(nonce, tmp_presentations);
+        SessionService.addToSession(sessionID, tmp_presentations);
 
         return Response.ok()
-                .entity("Presentation added to session " + nonce)
-                .link("http://localhost:8080/resource?nonce=" + nonce, "OID4VC-access-resource")
+                .entity("Presentation added to session " + sessionID)
                 .header("Content-Type", "text/html")
                 .build();
     }
@@ -199,7 +268,7 @@ public class VerServlet {
             return sessionId;
         }
 
-        // Adds a list of Verifiable Presentations to a session.
+        // Adds a list of Verifiable Presentations to a session ID.
         public static void addToSession(String sessionID, ArrayList<Model> verifiablePresentation) {
             session_presentations.put(sessionID, verifiablePresentation);
         }
@@ -213,6 +282,11 @@ public class VerServlet {
         public static boolean isSessionAuthorizedForResource(String sessionID, String path) {
             ArrayList<Model> presentationArrayList = session_presentations.get(sessionID);
             ArrayList<Model> shapeArrayList = resource_shapes.get(path);
+
+            if (presentationArrayList == null || shapeArrayList == null) {
+                return false;
+            }
+
             for (Model presentation : presentationArrayList) {
                 for (Model shape : shapeArrayList) {
                     if (AuthService.validateShape(shape, presentation)) {
@@ -247,7 +321,6 @@ public class VerServlet {
 
         // Validates a graph against a SHACL shape using the Jena SHACL API.
         public static boolean validateShape(Model shape, Model graph) {
-
             Graph shapeGraph = shape.getGraph();
             Graph dataGraph = graph.getGraph();
 
@@ -282,7 +355,6 @@ public class VerServlet {
 
     // Parses an RDF string into a Jena Model.
     public static Model parseRDF(String rdfString, Lang lang) {
-
         Model model = ModelFactory.createDefaultModel();
         try (StringReader stringReader = new StringReader(rdfString)) {
             RDFDataMgr.read(model, stringReader, null, lang);
